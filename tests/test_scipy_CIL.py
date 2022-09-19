6e12700a624e0bc47c093d8e90eeab3c2a6bbec4
nabr <nabr@dtu.dk>
2022-09-19 21:01:15 +0200

    Add more info on the test script

--- tests/test_scipy_CIL.py
+++ tests/test_scipy_CIL.py
@@ -7,3 +7,14 @@ from cil.framework import ImageGeometry, AcquisitionGeometry, DataContainer
 def test_dst():
     x = np.random.rand(10)
     y = dst(x)
+
+def test_CIL():
+    im_size = (45,45),
+    det_count = 50
+    angles = np.linspace(0,np.pi,60),
+    domain = im_size
+    det_spacing = 1
+
+    acquisition_geometry = AcquisitionGeometry.create_Parallel2D()
+    acquisition_geometry.set_angles(angles, angle_unit="radian")
+    acquisition_geometry.set_panel(det_count, pixel_size=det_spacing)
